36eff907aca25751dd4755700ea6bcd61676e946
arvilays <123064316+arvilays@users.noreply.github.com>
2025-01-30 01:51:42 -0800

    Partially implemented gameManager, gameBoard, and gameDisplay

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,41 @@
 const main = () => {
-    gameDisplay.render(gameBoard.getBoard());
+    gameManager.startGame();
+    // gameBoard.setMarker(0, 0, "X");
+    // gameBoard.setMarker(0, 1, "X");
+    // gameBoard.setMarker(0, 2, "X");
 }
+
+const gameManager = (function() {
+    let currentPlayer = "O";
+
+    const startGame = () => {
+        gameBoard.reset();
+        gameDisplay.render(gameBoard.getBoard());
+    }
+
+    const getCurrentPlayer = () => {
+        return currentPlayer;
+    }
+
+    const _nextPlayer = () => {
+        if (currentPlayer == "X") currentPlayer = "O";
+        else if (currentPlayer == "O") currentPlayer = "X";
+        events.trigger("playerChanged");
+    }
+
+    const _winGame = () => {
+        console.log(currentPlayer + " WINS!");
+    }
+
+    const _tieGame = () => {
+        console.log("TIE!");
+    }
+
+    events.subscribe("gameBoardChanged", _nextPlayer);
+    events.subscribe("gameWon", _winGame);
+    events.subscribe("gameTie", _tieGame);
+    return { startGame, getCurrentPlayer };
+})();
 
 const gameBoard = (function() {
     let board = [[" ", " ", " "],
@@ -19,78 +54,116 @@ const gameBoard = (function() {
     
     const getBoard = () => { return board; }
 
-    const setMarker = (symbol, row, column) => {
-        if (getMarker(row, column) == " ") {
-            board[row][column] = symbol;
+    const getMarker = (coords) => { return board[coords[0]][coords[1]]; }
+
+    const setMarker = (coords) => {
+        if (getMarker(coords) == " ") {
+            board[coords[0]][coords[1]] = gameManager.getCurrentPlayer();
             print();
-            _checkWin(symbol);
-            gameDisplay.render(getBoard());
+            events.trigger("gameBoardChanged", getBoard());
         }
         else console.log("Square already filled");
     }
-
-    const getMarker = (row, column) => { return board[row][column]; }
 
     const reset = () => {
         board = [[" ", " ", " "],
                  [" ", " ", " "],
                  [" ", " ", " "]];
+        events.trigger("gameBoardChanged", getBoard());
     }
 
-    const _checkWin = symbol => {
-        let gameResult = _verifyWin(symbol);
-        if (gameResult == "win") _win(symbol);
-        else if (gameResult == "tie") _tie();
-    }
-
-    const _verifyWin = symbol => {
+    const _verifyWin = () => {
+        let currentPlayer = gameManager.getCurrentPlayer();
         let boardLength = board.length;
         let isBoardFull = true;
         let firstDiagonalSymbolCount = 0; // Top left to bottom right diagonal
         let secondDiagonalSymbolCount = 0; // Bottom left to top right diagonal
         for (let i = 0; i < boardLength; i++) {
             // Check if row is all symbol
-            if (board[i].every((val, i, arr) => val === arr[0] && val == symbol)) return "win";
+            if (board[i].every((val, i, arr) => val === arr[0] && val == currentPlayer)) {
+                events.trigger("gameWon");
+                return;
+            }
 
             // Check if column is all symbol
             let columnSymbolCount = 0;
             for (let j = 0; j < boardLength; j++) {
-                if (board[j][i] == symbol) columnSymbolCount++;
+                if (board[j][i] == currentPlayer) columnSymbolCount++;
                 if (board[i][j] == " ") isBoardFull = false;
             }
-            if (columnSymbolCount == boardLength) return "win";
-            
+            if (columnSymbolCount == boardLength) {
+                events.trigger("gameWon");
+                return;
+            }
+
             // Check if both diagonals is all symbol
-            if (board[i][i] == symbol) firstDiagonalSymbolCount++;
-            if (board[i][boardLength - 1 - i] == symbol) secondDiagonalSymbolCount++;
+            if (board[i][i] == currentPlayer) firstDiagonalSymbolCount++;
+            if (board[i][boardLength - 1 - i] == currentPlayer) secondDiagonalSymbolCount++;
         }
 
-        if (firstDiagonalSymbolCount == boardLength) return "win";
-        else if (secondDiagonalSymbolCount == boardLength) return "win";
-        else if (isBoardFull) return "tie";
+        if (firstDiagonalSymbolCount == boardLength) {
+            events.trigger("gameWon", currentPlayer);
+            return;
+        } else if (secondDiagonalSymbolCount == boardLength) {
+            events.trigger("gameWon", currentPlayer);
+            return;
+        } else if (isBoardFull) {
+            events.trigger("gameTie");
+            return;
+        }
     }
 
-    const _win = symbol => {
-        console.log(symbol + " WINS!");
-    }
-
-    const _tie = () => {
-        console.log("TIE!");
-    }
-
+    events.subscribe("gameBoardChanged", _verifyWin);
+    events.subscribe("squareClicked", setMarker);
     return{ print, getBoard, setMarker, getMarker, reset };
 })();
 
 const gameDisplay = (function () {
     const squares = document.querySelectorAll(".square");
+    const player1 = document.querySelector(".player1");
+    const player2 = document.querySelector(".player2");
+    squares.forEach(item => {
+        item.addEventListener("click", () => {
+            squareClicked(item);
+        });
+    });
 
     const render = board => {
+        let boardSymbols = [];
         for (let i = 0; i < board.length; i++) {
-            console.log("test");
+            for (let j = 0; j < board[i].length; j++) {
+                boardSymbols.push(board[i][j]);
+            }
         }
+
+        let count = 0;
+        squares.forEach(item => {
+            item.textContent = boardSymbols[count];
+            count++;
+        });
     };
 
-    return { render };
+    const squareClicked = item => {
+        if (gameManager.getCurrentPlayer() == "O") {
+            item.style.color = "white";
+        }
+        events.trigger("squareClicked", [item.id[0], item.id[1]]);
+    }
+
+    const _changePlayer = () => {
+        let currentPlayer = gameManager.getCurrentPlayer();
+        if (currentPlayer == "X") {
+            player1.style.filter = "drop-shadow(0px 0px 30px white)";
+            player2.style.filter = "revert";
+        } else if (currentPlayer == "O") {
+            player2.style.filter = "drop-shadow(0px 0px 30px white)";
+            player1.style.filter = "revert";
+        }
+    }
+
+    events.subscribe("gameBoardChanged", render);
+    events.subscribe("playerChanged", _changePlayer);
+    return { render, squareClicked };
 })();
 
 main();
@@ -115,45 +188,7 @@ main();
 
 
 
-// gameBoard.reset();
-// gameBoard.setMarker("x", 0, 0);
-// gameBoard.setMarker("x", 0, 1);
-// gameBoard.setMarker("x", 0, 2);
 
-// gameBoard.reset();
-// gameBoard.setMarker("x", 1, 0);
-// gameBoard.setMarker("x", 1, 1);
-// gameBoard.setMarker("x", 1, 2);
-
-// gameBoard.reset();
-// gameBoard.setMarker("x", 2, 0);
-// gameBoard.setMarker("x", 2, 1);
-// gameBoard.setMarker("x", 2, 2);
-
-// gameBoard.reset();
-// gameBoard.setMarker("x", 0, 0);
-// gameBoard.setMarker("x", 1, 0);
-// gameBoard.setMarker("x", 2, 0);
-
-// gameBoard.reset();
-// gameBoard.setMarker("x", 0, 1);
-// gameBoard.setMarker("x", 1, 1);
-// gameBoard.setMarker("x", 2, 1);
-
-// gameBoard.reset();
-// gameBoard.setMarker("x", 0, 2);
-// gameBoard.setMarker("x", 1, 2);
-// gameBoard.setMarker("x", 2, 2);
-
-// gameBoard.reset();
-// gameBoard.setMarker("x", 0, 0);
-// gameBoard.setMarker("x", 1, 1);
-// gameBoard.setMarker("x", 2, 2);
-
-// gameBoard.reset();
-// gameBoard.setMarker("x", 0, 2);
-// gameBoard.setMarker("x", 1, 1);
-// gameBoard.setMarker("x", 2, 0);
 
 /*
 WIN CONDITIONS INCLUDE
